0b7d4891d6f769ded2d4f2c51e5a05f9f3bb0e5d
Roman Tudvasev <Roman.Tudvasev@endava.com>
2020-08-13 16:42:43 +0300

    Added DocDB final snapshot identifier

--- tf/modules/document_db/main.tf
+++ tf/modules/document_db/main.tf
@@ -56,8 +56,9 @@ resource "aws_docdb_cluster" "articles_document_db_cluster" {
     db_subnet_group_name    = aws_docdb_subnet_group.docdb_subnets.id
     vpc_security_group_ids  = [aws_security_group.docdb_access.id]
 
-    deletion_protection = "true"
-    skip_final_snapshot = true
+    deletion_protection         = true
+    skip_final_snapshot         = true
+    final_snapshot_identifier   = "hive-docdb-curie-fs"
 }
 
 resource "aws_docdb_cluster_instance" "articles_docdb_instance" {
