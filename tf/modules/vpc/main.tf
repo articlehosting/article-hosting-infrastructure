a041f1e2b081cda5acf8a36c30c30404c61f4af4
Gleb Godonoga <gleb.godonoga@gmail.com>
2020-08-17 16:04:10 +0300

    Adds a 3rd subnet to vpc

--- tf/modules/vpc/main.tf
+++ tf/modules/vpc/main.tf
@@ -9,7 +9,7 @@ module "vpc" {
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = []
-  public_subnets       = ["10.0.3.0/24", "10.0.4.0/24"]
+  public_subnets       = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
   enable_dns_hostnames = true
 
   tags               = var.tags
