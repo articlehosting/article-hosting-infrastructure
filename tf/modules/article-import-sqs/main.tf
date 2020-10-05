df7b6728be168ffbdccf58b20b542592ab23a307
Roman Tudvasev <Roman.Tudvasev@endava.com>
2020-10-05 13:25:24 +0300

    Added S3 notifications for meca files

--- tf/modules/article-import-sqs/main.tf
+++ tf/modules/article-import-sqs/main.tf
@@ -49,6 +49,17 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   }
 }
 
+resource "aws_s3_bucket_notification" "bucket_notification_2" {
+  bucket = aws_s3_bucket.import_bucket.id
+
+  queue {
+    id              = "new-article-upload"
+    queue_arn       = aws_sqs_queue.import_queue.arn
+    events          = ["s3:ObjectCreated:*"]
+    filter_suffix   = ".meca"
+  }
+}
+
 data "aws_sqs_queue" "data_import_queue" {
   name  = aws_sqs_queue.import_queue.name
 }
